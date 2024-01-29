f76e171a65d0988630eb99117fa9db00b89eb710
Muhammad Ahsan Naeem <ahsannaeem480@gmail.com>
2024-01-29 15:22:39 +0500

    Protect the Listing Page Route That could not be visible until unless we loggin in site

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgotpassword" element={<ForgetPassword />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
